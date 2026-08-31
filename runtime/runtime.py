c6a618449dfa909ffb77e3d8ed950cbeaaf2c3df
Bambam <gabrielbambamfabiyi@outlook.com>
2026-08-31 09:18:25 +0000

    added runtime manager execute method

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -2,14 +2,26 @@ import os
 from typing import Any
 from src.harness.artefacts.action import ActionArtefact
 from src.harness.artefacts.execute import ExecutionArtefact
+from uuid import UUID
 
 class RuntimeManager:
-    def __init__(self, agent, model, tools, execution_dir, execution_id):
+    def __init__(self, agent: str, model: str, tools: dict[str. Any], executions_dir: str):
         self.agent = agent
         self.model = model
         self.tools = tools
-        self.execution_dir = execution_dir
-        self.execution_id = execution_id
+        self.executions_dir = execution_dir
 
-    def execute(self, tool: dict[str, Any], action_artefact) -> ExecutionArtefact:
-        pass
+    def execute(self, tool: dict[str, Any], action_artefact: ActionArtefact, execution_id: UUID) -> ExecutionArtefact:
+        
+        runtime_path = self._prepare_execution_environment(action_artefact.execution_id)
+
+        container = self._get_or_create_container(action_artefact.execution_id, runtime_path)
+
+        try:
+            result = self._execute_tool(container, tool, action_artefact.input)
+
+            return self._create_execution_artefact(action_artefact, result)
+
+        except Exception as e:
+            
+            return self._create_execution_artefact(action_artefact, e)
